6c0b48c0a78746354c93036ad6c4a75e482990a5
Joseph Clark <joseph@jclark.codes>
2023-01-26 08:16:31 -0500

    Alignment, logic

--- module3/lab3_part2.py
+++ module3/lab3_part2.py
@@ -6,14 +6,23 @@ print("Welcome to the CMSY 156 Soccer Calculator!")
 
 games = float(input("Enter the number of games: "))
 shots = float(input("Enter the number of shots taken: "))
+if (games == 0 or shots == 0):
+    print(f"The average goals per game is: 0.0")
+    print(f"The average shots per game is: 0.0")
+    print(f"The average shots per goal is: 0.0")
+    exit()
 goals = float(input("Enter the number of goals made: "))
 
-average_goals = goals / games
-average_shots = shots / games
-average_shots = shots / goals
+if (goals == 0):
+    average_shots_per_goal = 0.0
+else:
+    average_shots_per_goal = shots / goals
 
-print(f"The average goals per game is : {average_goals:>,.2f}")
-print(f"The average shots per game is: {average_shots:>,.2f}")
-print(f"The average shots per goal is: {average_shots:>,.2f}")
+average_goals = goals / games
+average_shots_per_game = shots / games
+
+print(f"The average goals per game is: {average_goals:>20,.2f}")
+print(f"The average shots per game is: {average_shots_per_game:>20,.2f}")
+print(f"The average shots per goal is: {average_shots_per_goal:>20,.2f}")
 
 print("Thank you for using this program!")  
