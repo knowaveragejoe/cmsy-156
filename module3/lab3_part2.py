cc710248a81b071e75788fe7940cd73cd93afdbc
Joseph Clark <joseph@jclark.codes>
2023-01-26 08:32:59 -0500

    comments

--- module3/lab3_part2.py
+++ module3/lab3_part2.py
@@ -4,6 +4,7 @@
 
 print("Welcome to the CMSY 156 Soccer Calculator!")
 
+# Collect user input and handle 0-cases
 games = float(input("Enter the number of games: "))
 shots = float(input("Enter the number of shots taken: "))
 if (games == 0 or shots == 0):
@@ -13,6 +14,7 @@ if (games == 0 or shots == 0):
     exit()
 goals = float(input("Enter the number of goals made: "))
 
+# Ensure shots per goal is 0 if no goals were scored
 if (goals == 0):
     average_shots_per_goal = 0.0
 else:
@@ -21,6 +23,7 @@ else:
 average_goals = goals / games
 average_shots_per_game = shots / games
 
+# Print output
 print(f"The average goals per game is: {average_goals:>20,.2f}")
 print(f"The average shots per game is: {average_shots_per_game:>20,.2f}")
 print(f"The average shots per goal is: {average_shots_per_goal:>20,.2f}")
