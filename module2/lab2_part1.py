ed0c293d5df5524039b9673c23d53bee2470f6dc
Joseph Clark <joseph@jclark.codes>
2023-02-01 00:51:13 -0500

    updating lab2 part1

--- module2/lab2_part1.py
+++ module2/lab2_part1.py
@@ -4,16 +4,18 @@
 
 print("Welcome to the CMSY 156 Soccer Calculator!")
 
+# Take user input
 games = int(input("Enter the number of games: "))
 shots = int(input("Enter the number of shots taken: "))
 goals = int(input("Enter the number of goals made: "))
 
+# Perform our calculations
 average_goals = goals / games
 average_shots_per_game = shots / games
 average_shots_per_goal = shots / goals
 
+# Show output & exit
 print("The average goals per game is : " + str(average_goals))
 print("The average shots per game is: " + str(average_shots_per_game))
 print("The average shots per goal is: " + str(average_shots_per_goal))
-
 print("Thank you for using this program!")
