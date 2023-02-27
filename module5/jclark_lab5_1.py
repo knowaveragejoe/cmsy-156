f2842f4ccf3d35265438a7a3fbab6b913bc80d98
Joseph Clark <joseph@jclark.codes>
2023-02-26 22:29:04 -0500

    module 5 lab 1 complete

--- module5/jclark_lab5_1.py
+++ module5/jclark_lab5_1.py
@@ -1,7 +1,54 @@
+# CMSY-156
 # Programmer name: Joseph Clark 
 # 02/02/22 
 # Lab 3: Decision Making - part 1
 
+# Constants
+SALES_TAX = 0.06
+PLAIN_COST = 11.50
+PEPPERONI_COST = 12.50
+VEGGIE_COST = 13.50
+
+# Displays the pizza menu
+def display_pizza_menu():
+    print("""
+        1. Plain Pizza
+        2. Pepperoni Pizza
+        3. Veggie Pizza
+        4. Checkout
+    """)
+
+# Returns a human-readable string of what pizza the user selected
+def display_pizza_selection(pizza_type):
+    return "Plain Pizza" if pizza_type == "1" else "Pepperoni Pizza" if pizza_type == "2" else "Veggie Pizza" if pizza_type == "3" else "Invalid Pizza"
+
+# Displays a generic error message
+def display_error(error):
+    return "There was an issue with your order: " + error
+
+# Asks for a tip, then calculates totals and prints the receipt
+def checkout(total, delivery_type, address): 
+    # Collect tip and add to total if valid
+    tip_is_valid = False
+    while (not tip_is_valid):
+        tip = input("Please enter the amount of the tip: $")
+        if (not tip or not tip.isnumeric()):
+            display_error("Tip entered is invalid. Please try again")
+        else:
+            tip_is_valid = True
+            total += float(tip)
+    
+    # Calculate tax and add to total
+    total += total * SALES_TAX
+    
+    # Display receipt
+    if (delivery_method == "1"):
+        print(f"The total cost with tax, tip and delivery charge is: ${total:,.2f}")
+        print("Delivery address: " + delivery_address)
+    else:
+        print(f"The total cost with tax and tip is: ${total:,.2f}")
+
+####### Actual program ########
 print("Welcome to the CMSY-156 Pizza Shop!")
 
 # Order total
@@ -14,23 +61,13 @@ while (not delivery_method_valid):
     if (delivery_method == "1" or delivery_method == "2"):
         delivery_method_valid = True
     else:
-        print("Error: Please enter 1 or 2. Try again!")
+        display_error("Please enter 1 or 2. Try again!")
 
 # Collect address if delivery selected & add delivery charge
 delivery_address = ""
 if (delivery_method == "1"):
     total += 5.00
     delivery_address = input("Enter the delivery address: ")
-
-# Collect tip and add to total if valid
-tip_is_valid = False
-while (not tip_is_valid):
-    tip = input("Please enter the amount of the tip: $")
-    if (not tip or not tip.isnumeric()):
-        print("Tip entered is invalid. Please try again")
-    else:
-        tip_is_valid = True
-        total += float(tip)
 
 # Begin collecting order info
 print("What would you like to order today?")
@@ -40,12 +77,7 @@ pizza_total = 0.00
 pizza_type_valid = False
 while (not pizza_type_valid):
     print("What would you like to order today?")
-    print("""
-        1. Plain Pizza
-        2. Pepperoni Pizza
-        3. Veggie Pizza
-        4. Checkout
-    """)
+    display_pizza_menu()
     pizza_selection = input("Enter your order here: ")
     
     # Accumulate pizza orders, ff the user selects checkout we break out of loop
@@ -53,26 +85,22 @@ while (not pizza_type_valid):
         break
     else:
         if (pizza_selection == "1"):
-            pizza_cost = 11.50
+            pizza_cost = PLAIN_COST
         elif (pizza_selection == "2"):
-            pizza_cost = 12.50
+            pizza_cost = PEPPERONI_COST
         elif (pizza_selection == "3"):
-            pizza_cost = 13.50
+            pizza_cost = VEGGIE_COST
         else:
-            print("Error: please enter a valid menu option. Try again!")
+            display_error("Please enter a valid menu option. Try again!")
             continue
         
         pizza_total += pizza_cost
 
-# Calculate tax & final total
-tax = pizza_total * 0.06
-total = total + pizza_total + tax
+# Add the cost of the pizzas to our running total
+total += pizza_cost
 
-# Display output & end interaction with user
-if (delivery_method == "1"):
-    print(f"The total cost with tax, tip and delivery charge is: ${total:,.2f}")
-    print("Delivery address: " + delivery_address)
-else:
-    print(f"The total cost with tax and tip is: ${total:,.2f}")
+# Call checkout
+checkout(total, delivery_method, delivery_address)
 
+# Display exit message
 print("Thank you for coming to the CMSY-156 pizza shop. Please come again!")
